134dd9a7bb725c4687f89c843a6edc009a234d03
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-03-15 11:54:59 +0700

    Praktikum 2.2 - JS 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,22 +24,16 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke table m_user
         
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::insert($data);
-        
         // coba akses model UserModel
         //$user = UserModel::all(); // ambil semua data dari tabel m_user
         //$user = UserModel::find(1); // ambil data dari tabel m_user dengan id = 1
         //$user = UserModel::where('level_id', 1)->first(); // ambil data dari tabel m_user dengan level_id = 1
         //$user = UserModel::firstWhere('level_id', 1); // ambil data dari tabel m_user dengan level_id = 1 dan ambil data pertama
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        }); // ambil data dari tabel m_user dengan id = 1, jika tidak ada, tampilkan error 404
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // }); // ambil data dari tabel m_user dengan id = 1, jika tidak ada, tampilkan error 404
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
