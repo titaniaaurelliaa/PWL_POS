03b8936e7d36801c37275002e581f56ca54bc72e
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-03-20 14:59:27 +0700

    Praktikum 2 - JS 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="card-tools"></div>
     </div>
     <div class="card-body">
-        Selamat datamg semua, ini adalah halaman utama dari aplikasi ini.
+        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
     </div>
 </div>
 @endsection
