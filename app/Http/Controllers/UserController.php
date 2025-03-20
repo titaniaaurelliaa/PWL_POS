995a043371d1439e89d48776c0840074670b24b8
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-03-20 20:27:59 +0700

    Praktikum 4 - JS 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,15 +25,21 @@ class UserController extends Controller
 
         $activeMenu = 'user';
 
-        $level = LevelModel::all();
+        $level = LevelModel::all(); // ambil data level untuk filter level
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu, 'page' => $page, 'level' => $level]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
+    // Ambil data user dalam bentuk json untuk datatables
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
 
+        // Filter berdasarkan level
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+        
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
