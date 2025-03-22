916190db8eb461c176c745f9544fd43cc23900ff
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-03-22 13:58:28 +0700

    Praktikum 3 - JS 6

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); //menampilkan form confirm delete user ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // menghapus data user ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
 });
 
