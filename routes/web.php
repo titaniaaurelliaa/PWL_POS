c65004dfd68ec296681dbc7075eb4ae09b9428ff
titaniaaurelliaa <titaniaaurelliaputri@gmail.com>
2025-03-22 14:00:12 +0700

    Praktikum 3 - JS 6

--- routes/web.php
+++ routes/web.php
@@ -41,13 +41,20 @@ Route::group(['prefix' => 'user'], function () {
     Route::post("/list", [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    
+    // Create dengan ajax
     Route::get('/create_ajax', [UserController::class, 'create_ajax']); // menampilkan halaman form tambah user ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']); // menyimpan data user baru ajax
+    
     Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user ajax
+    
+    // Edit dengan ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user ajax
+    
+    // Delete dengan ajax
     Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); //menampilkan form confirm delete user ajax
     Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // menghapus data user ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
